2443b0dd8028c3c42c01e2a8d0372b1f49c9832d
Ilir Maci <ilir.maci@gmail.com>
2014-03-30 13:55:44 +0200

    Removing first assignment during course

--- Assignment1/getmonitor.R
+++
@@ -1,6 +0,0 @@
-# function to read monitor data and return a data frame
-getmonitor <- function(directory, mon){
-    mon.name <- paste0(sprintf('%03d', mon), '.csv')
-    mon.path <- file.path(directory, mon.name)
-    read.csv(mon.path)
-}
